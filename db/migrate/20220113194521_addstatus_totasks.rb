b0f7494c9071a7a5ddc0bcb3e56589f112bf8413
kouhei.nogawa <nk04084163@gmail.com>
2022-01-13 20:07:59 +0000

    kadai2

---
+++ db/migrate/20220113194521_addstatus_totasks.rb
@@ -0,0 +1,5 @@
+class AddstatusTotasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_065307) do
+ActiveRecord::Schema.define(version: 2022_01_13_194521) do
 
   create_table "task_lists", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_065307) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
