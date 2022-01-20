ecdd1157ae3734b665a2769ce90e9da97a1467d1
kouhei.nogawa <nk04084163@gmail.com>
2022-01-20 00:27:57 +0000

    kadai-tasklist3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
     private
     
     def set_task
-        @task = Task.find(oarams[:id])
+        @task = Task.find(params[:id])
     end
     
     # Strong Parameter
